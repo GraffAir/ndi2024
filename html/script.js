c7fe8843f9f993ddb76d4ffce520935c01652969
Corentin Finou Win <corentin.finou@free.fr>
2024-12-05 21:34:52 +0100

    debut js

--- html/script.js
+++ html/script.js
@@ -1,3 +1,9 @@
 window.changementDePage = function(href){
-    window.location.href = href
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 500)
 }
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
+})
