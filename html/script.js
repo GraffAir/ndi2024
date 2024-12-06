b47bf0f8388a528ca21827ce52debd312533f277
GraffAir <123460802+GraffAir@users.noreply.github.com>
2024-12-06 02:29:51 +0100

    allez on push tout

--- html/script.js
+++ html/script.js
@@ -1,9 +1,14 @@
 window.changementDePage = function(href){
-    document.querySelector('body').style.opacity = 0
+    const filler = document.querySelector('.screen-filler');
+    filler.style.opacity = 1; // Assombrit l'écran
+    filler.style.visibility = 'visible'; // Rends l'écran visible
     setTimeout(function() { 
-        window.location.href = href
-    }, 500)
+        window.location.href = href; // Change de page après 700ms (ou un peu plus)
+    }, 700); // Assurez-vous que ce délai est supérieur à la durée de la transition CSS (.5s ici)
 }
-document.addEventListener('DOMContentLoaded', function(event) {
-    document.querySelector('body').style.opacity = 1
-})
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.screen-filler').style.opacity = 0;
+    setTimeout(function() {
+        document.querySelector('.screen-filler').style.visibility = 'hidden';
+    }, 700);
+});
